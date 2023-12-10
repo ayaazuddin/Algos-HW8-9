e58d7fae92dcb20339eba31a04c3f781c748d3a9
ayaazuddin <mohammad.ay@northeastern.edu>
2023-12-10 12:42:52 -0500

    Final

--- BinomialHeap.py
+++ BinomialHeap.py
@@ -8,166 +8,192 @@ class BinomialNode:
 
 class BinomialHeap:
     def __init__(self):
-        self.head = None
+        self.orderlist = [None]*1000
+        self.min = BinomialNode(float("inf"))
 
-    def make_heap(self):
-        return BinomialHeap()
+    def make_heap(self, key):
+        return BinomialNode(key)
+
+    def Min(self):
+        return self.min
 
     def insert(self, key):
-        new_heap = self.make_heap()
-        new_heap.head = BinomialNode(key)
-        self.head = self.union(self, new_heap)
-
-    def minimum(self):
-        if not self.head:
-            return None
-
-        min_node = self.head
-        current = self.head.sibling
-
-        while current:
-            if current.key < min_node.key:
-                min_node = current
-            current = current.sibling
-
-        return min_node.key
-
-    def extract_min(self):
-        if not self.head:
-            return None
-
-        min_node = self.head
-        prev = None
-        prev_next = None
-        current = self.head.sibling
-
-        while current:
-            if current.key < min_node.key:
-                min_node = current
-                prev = prev_next
-            prev_next = current
-            current = current.sibling
-
-        if prev:
-            prev.sibling = min_node.sibling
+        node = self.make_heap(key)
+        if node.key < self.min.key:
+            self.min = node
+        if self.orderlist[0] is not None:
+            self.merge(node, self.orderlist[0])
         else:
-            self.head = min_node.sibling
-
-        child = min_node.child
-        while child:
-            next_child = child.sibling
-            child.sibling = None
-            new_heap = self.make_heap()
-            new_heap.head = child
-            self.head = self.union(self, new_heap)
-            child = next_child
-
-        return min_node.key
-
-    def union(self, h1, h2):
-        new_heap = self.make_heap()
-        new_heap.head = self.merge(h1.head, h2.head)
-        self.head = None
-        h2.head = None
-        return self.adjust_heap(new_heap)
-
-    def decrease_key(self, node, new_key):
-        if new_key > node.key:
-            print("New key is greater than current key.")
-            return
-
-        node.key = new_key
-        while node.parent and node.key < node.parent.key:
-            node.key, node.parent.key = node.parent.key, node.key
-            node = node.parent
-
-    def delete(self, node):
-        self.decrease_key(node, float('-inf'))
-        self.extract_min()
+            self.orderlist[0] = node
 
     def merge(self, h1, h2):
-        if not h1:
-            return h2
-        if not h2:
-            return h1
-
-        head = None
-        tail = None
-        while h1 and h2:
-            if h1.degree <= h2.degree:
-                next_heap = h1
-                h1 = h1.sibling
+        if h1.key < h2.key:
+            h2.parent = h1
+            h2.sibling = h1.child
+            h1.child = h2
+            self.orderlist[h1.degree] = None
+            h1.degree += 1
+            if len(self.orderlist) <= h1.degree:
+                self.orderlist.append(h1)
             else:
-                next_heap = h2
-                h2 = h2.sibling
-
-            if not head:
-                head = next_heap
-                tail = next_heap
-            else:
-                tail.sibling = next_heap
-                tail = next_heap
-
-        if h1:
-            tail.sibling = h1
-        else:
-            tail.sibling = h2
-
-        return head
-
-    def adjust_heap(self, heap):
-        current = heap.head
-        prev = None
-        next_node = current.sibling
-
-        while next_node:
-            if (current.degree != next_node.degree or
-                    (next_node.sibling and next_node.sibling.degree == current.degree)):
-                prev = current
-                current = next_node
-            else:
-                if current.key <= next_node.key:
-                    current.sibling = next_node.sibling
-                    next_node.parent = current
-                    next_node.sibling = current.child
-                    current.child = next_node
-                    current.degree += 1
+                if self.orderlist[h1.degree] is None:
+                    self.orderlist[h1.degree] = h1
                 else:
-                    if not prev:
-                        heap.head = next_node
-                    else:
-                        prev.sibling = next_node
-                    current.parent = next_node
-                    current.sibling = next_node.child
-                    next_node.child = current
-                    next_node.degree += 1
-                    current = next_node
+                    temp = self.orderlist[h1.degree]
+                    self.orderlist[h1.degree] = None
+                    self.merge(h1,temp)
 
-            next_node = current.sibling
+        else:
+            h1.parent = h2
+            h1.sibling = h2.child
+            h2.child = h1
+            self.orderlist[h2.degree] = None
+            h2.degree += 1
+            if len(self.orderlist) <= h2.degree:
+                self.orderlist.append(h2)
+            else:
+                if self.orderlist[h2.degree] is None:
+                    self.orderlist[h2.degree] = h2
+                else:
+                    temp = self.orderlist[h2.degree]
+                    self.orderlist[h2.degree] = None
+                    self.merge(h2,temp)
+    
+    def ExtractMin(self):
+        pointer = self.min
+        print("Extracted Minimum:",pointer.key)
+        if pointer.degree == 0:
+            self.orderlist[0] = None
+        elif pointer.degree == 1:
+            if self.orderlist[0] is None:
+                self.orderlist[0] = pointer.child
+                self.orderlist[pointer.degree] = None
+            else:
+                # print(pointer.child.key, self.orderlist[0].key)
+                self.orderlist[pointer.degree] = None
+                self.merge(pointer.child,self.orderlist[0])
+        else:
+            lis = []
+            temp = pointer.child
+            while temp:
+                lis.append(temp)
+                temp = temp.sibling
+            self.orderlist[pointer.degree] = None
+            for h in lis:
+                h.sibling = None
+                h.parent = None
+                if self.orderlist[h.degree] is None:
+                    self.orderlist[h.degree] = h
+                else:
+                    self.merge(h,self.orderlist[h.degree])
+            
 
-        return heap.head
+        self.min = BinomialNode(float("inf"))
+        for h in self.orderlist:
+            if h!=None and h.key < self.min.key:
+                self.min = h
+        
+                
+    def delete(self,key):
+        i = self.decreaseKey(key,float("-inf"))
+        self.min = self.orderlist[i]
+        self.ExtractMin()
+
+    def decreaseKey(self,old,new):
+        idx = 0
+        for i,h in enumerate(self.orderlist):
+            x = self.findNode(h,old)
+            if h is not None and x is not None:
+                node = x
+                idx = i
+        
+        if node is None:
+            return
+        
+        node.key = new
+        parent = node.parent
+        while parent is not None and node.key < parent.key:
+            temp = node.key
+            node.key = parent.key
+            parent.key = temp
+            node = parent
+            parent = parent.parent
+
+        return idx
+        
+    def findNode(self, h, key):
+        if (h == None):
+            return None
+    
+        # check if key is equal to the root's data
+        if (h.key == key):
+            return h
+    
+        # Recur for child
+        res = self.findNode(h.child, key)
+        if (res != None):
+            return res
+    
+        return self.findNode(h.sibling, key)
+    
+    def print_binomial_heap(self, heap):
+        for i, tree in enumerate(heap.orderlist):
+            if tree is not None:
+                print(f"Trees of order {i}:")
+                self.print_tree(tree)
+                print()
+
+    def print_tree(self, node, depth=0):
+        if node is not None:
+            print("  " * depth + f"{node.key} (degree {node.degree})")
+            self.print_tree(node.child, depth + 1)
+            self.print_tree(node.sibling, depth)
 
 
-# Example usage:
-heap = BinomialHeap()
+if __name__ == "__main__":
+    binomial_heap = BinomialHeap()
 
-# Insert some keys
-keys_to_insert = [5, 7, 2, 10, 3]
-for key in keys_to_insert:
-    heap.insert(key)
+    # Insert some elements
+    elements_to_insert = [7,2,4,17,1,11,6,8,15,10,20,5]
+    for element in elements_to_insert:
+        binomial_heap.insert(element)
 
-print("Heap after insertions:")
-print("Minimum:", heap.minimum())
-print("Extracted Min:", heap.extract_min())
-print("Heap after extracting min:")
-print("Minimum:", heap.minimum())
+    binomial_heap.print_binomial_heap(binomial_heap)
+    print("###############")
+    # print(binomial_heap.orderlist[2].child.sibling.parent.key)
 
-# Decrease key and delete
-node_to_decrease = heap.head.child
-heap.decrease_key(node_to_decrease, 1)
-print("Heap after decreasing key:")
-print("Minimum:", heap.minimum())
 
-heap.delete(node_to_decrease)
-print("Heap after deleting node:")
-print("Minimum:", heap.minimum())
+    binomial_heap.ExtractMin()
+    binomial_heap.print_binomial_heap(binomial_heap)
+    print("###############")
+    binomial_heap.decreaseKey(15,1)
+    binomial_heap.print_binomial_heap(binomial_heap)
+    print("###############")
+    binomial_heap.delete(1)
+    binomial_heap.print_binomial_heap(binomial_heap)
+    print("###############")
+    binomial_heap.delete(2)
+    # binomial_heap.ExtractMin()
+    binomial_heap.print_binomial_heap(binomial_heap)
+    # print("###############")
+    # binomial_heap.ExtractMin()
+    # binomial_heap.print_binomial_heap(binomial_heap)
+    # print("###############")
+    # binomial_heap.ExtractMin()
+    # binomial_heap.print_binomial_heap(binomial_heap)
+    # print("###############")
+    # print(binomial_heap.ExtractMin())
+    # binomial_heap.print_binomial_heap(binomial_heap)
+    # # Create a new binomial heap for testing merge
+    # binomial_heap_2 = BinomialHeap()
+    # elements_to_insert_2 = [10, 9, 12, 11]
+    # for element in elements_to_insert_2:
+    #     binomial_heap_2.insert(element)
+
+    # print("\nBinomial Heap 2:")
+    # binomial_heap.print_binomial_heap(binomial_heap_2)
+
+    # # Merge binomial_heap and binomial_heap_2
+
+    # print("\nBinomial Heap after Merging with Binomial Heap 2:")
+    # binomial_heap.print_binomial_heap(binomial_heap)
